7a396c6c95c367322ef6ff23265f5443295ceabc
sudo430 <tyler@bee-eee.com>
2024-01-08 15:34:38 -0700

    did csharp-prep1 and messed around in the sandbox

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        // asks for the users first name and writes it the 'firstName' variable
+        Console.Write("What is your first name? ");
+        string firstName = Console.ReadLine();
+
+        // asks for the users last name and writes it the 'lastName' variable
+        Console.Write("What is your last name? ");
+        string lastName = Console.ReadLine();
+        
+        // prints the users name
+        Console.WriteLine($"\nYour name is {lastName}, {firstName} {lastName}.");
     }
 }
--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -5,5 +5,11 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Sandbox World!");
+        Console.Write("enter a string >>");
+        string input = Console.ReadLine();
+        if (input == "no"){
+            Console.WriteLine("Don't tell me no");
+        }
+        Console.WriteLine($"\n{input}");
     }
 }
